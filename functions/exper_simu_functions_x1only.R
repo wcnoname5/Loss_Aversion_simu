38d299f6795830141e260ddb128f831d2967394d
wcnoname5 <wcnoname5@gmail.com>
2024-05-26 17:20:38 +0800

    New simulation function "x1_only" modified

--- functions/exper_simu_functions_x1only.R
+++ functions/exper_simu_functions_x1only.R
@@ -35,9 +35,9 @@ Player <- R6Class("Player",
       #               ifelse(prospect_dif==0,rbinom(1,1,.5),FALSE))}
       # Softmax:
       cho_A_prob <- 1/( 1+exp(private$phi * prospect_dif) ) 
-      cA <- ifelse(is.na(private$phi), prospect_dif<0,
+      choose_A <- ifelse(is.na(private$phi), prospect_dif<0,
                    rbinom(1,1, cho_A_prob)) #dif ==0, choose A
-      if (cA){
+      if (choose_A){
         return("A")
       }else{
         return("B")
@@ -70,16 +70,16 @@ Player <- R6Class("Player",
     }
   ),
   public = list(
-    initialize = function(alpha, beta,
-                          lambda, phi,
-                          wp, wn,
+    initialize = function(params,
+                          # alpha, beta, lambda, wp, wn,
+                          phi,
                           utility_function = c("CRRA","CARA")) {
-      private$alpha <- alpha 
-      private$beta <- beta  
-      private$lambda <- lambda 
+      private$alpha <- params["alpha"] 
+      private$beta <- params["beta"]  
+      private$lambda <- params["lambda"] 
+      private$wp <- params["wp"]
+      private$wn <- params["wn"]
       private$phi <- phi 
-      private$wp <- wp
-      private$wn <- wn
       private$utility_function <-  match.arg(utility_function)
     },
     input_choice = function(lotteries) {
@@ -127,18 +127,6 @@ Lotteries <- R6Class("Lotteries",
        } else if (trial == 3) {  # x1neg
          private$.A[2] <- L
          private$.B <- rep(given_values[2], 2)
-       } else if (trial == 4) {  # L2
-         private$.A[2] <- l
-         private$.B <- c(given_values[1], given_values[2])
-       } else if (trial == 5) {  # G2
-         private$.A[1] <- g
-         private$.B <- c(given_values[2], given_values[1])
-       } else if (trial <= 12 & trial >= 6 ) {  # xipos, trial: 6-12
-         private$.A <- c(given_values[1], l)
-         private$.B <- c(given_values[2], given_values[3])
-       } else {  # xineg, trial: 13-19
-         private$.A <- c(g, given_values[1])
-         private$.B <- c(given_values[4], given_values[2])
        }
      }
    ),
@@ -179,20 +167,25 @@ Lotteries <- R6Class("Lotteries",
 
 ### 1.3 Game
 
+
+# TODO: change n_trials to 3 ----------------------------------------------
+
 Game <- R6Class("Game",
     private = list(
       # Attributes
-      init_values = c(2000L, -2000L, 300L, -300L, 1000L),  # G, L, g, l, x1pos
-      n_trial = 19L, # 5+ 7 + 7
-      n_task = 5L,
+      # TODO variable init value and sequence
+      init_values = c("G"= 2000L, "L"=-2000L, "g"=300L, "l"=-300L, "x1+"=1000L),  # G, L, g, l, x1pos
+      n_trial = 3L, #19L, # = 5+ 7 + 7
+      n_task = 5L, # times of bisection
       task_log = NA,  # list of vectors, should be initialized
       lotteries_box = NA,  # 2D list, should be initialized
       # Methods
-      compute_value = function(choice, value_list, cur_task, cur_trial) {
+      ## bisection
+      bisection_update = function(choice, value_list, cur_task, cur_trial) {
         # choice <- "A" or "B"
         # value_list: e.g., x1pos_list
         # length(value_list) <- 6
-        target <- value_list[cur_task]
+        target <- value_list[cur_task] 
         step <- abs(value_list[1])
         change <- ((1 / 2) ^ cur_task) * step
         
@@ -207,7 +200,7 @@ Game <- R6Class("Game",
         }
         
         target <- as.integer(round(target / 5) * 5)  # Make target divisible by 5
-        return(target)
+        return(target) # return bisection iteration value
       }
     ),
     public = list(
@@ -226,30 +219,16 @@ Game <- R6Class("Game",
       show_setting = function() {
         # Output how many trials and tasks there are in this game
         setting <- c(
-          private$n_trial,
-          private$n_task,
-          private$init_values
+          "N_est" = private$n_trial,
+          "N_bisect" = private$n_task,
+          "Init" = private$init_values
         )
         return(setting)
       },
       
       # Generate a new pair of lotteries.
       generate_lotteries = function(cur_trial, cur_task) {  
-        # Prepare values that are needed when generating lotteries
-        L2 <- private$task_log[[4]][6]
-        G2 <- private$task_log[[5]][6]
-        needed_values <- c(rep(NA, 2), L2, G2)
-        # Find needed_values[1] depending on the demand of current trial
-        if (4 <= cur_trial & cur_trial <= 5) {  # L2 and G2
-          needed_values[1] <- private$task_log[[cur_trial - 2]][6]  # The last element of last trial
-        } else if (cur_trial == 5) {  # x2pos
-          needed_values[1] <- private$task_log[[2]][6]
-        } else if (cur_trial == 13) {  # x2neg
-          needed_values[1] <- private$task_log[[3]][6]
-        } else if (cur_trial > 5) {  # xipos and xineg, trial:6-12 & 14-19
-          needed_values[1] <- private$task_log[[cur_trial - 1]][6]
-        }
-        
+        needed_values <- rep(NA, 2)
         # Find needed_values[2] depending on current task
         needed_values[2] <- private$task_log[[cur_trial]][cur_task]
         
@@ -257,22 +236,22 @@ Game <- R6Class("Game",
         new_lotteries <- Lotteries$new(self, cur_trial, cur_task, needed_values)
         return(new_lotteries)
       },
+      
       update_lotteries_box = function(lotteries, cur_trial, cur_task) {
         private$lotteries_box[[cur_trial, cur_task]] <- lotteries
       },
+      
       update_task_log = function(choice, cur_trial, cur_task) {
         # The function takes the choice of players, does the computation, and update the task log.
         # Update the current value
         target_list <- private$task_log[[cur_trial]]
-        value <- private$compute_value(choice, target_list, cur_task, cur_trial)
+        value <- private$bisection_update(choice, target_list, cur_task, cur_trial) 
         private$task_log[[cur_trial]][cur_task + 1] <- value
         
         # Update next trial's initial value
         if (cur_task == 5) {  # Updating values after the final task of this trial
           g <- private$init_values[3]
           l <- private$init_values[4]
-          L2 <- private$task_log[[4]][6]
-          G2 <- private$task_log[[5]][6]
           
           if (cur_trial == 2) {  # x1pos, initialize x1neg[1]
             L <- private$task_log[[1]][6]
@@ -280,33 +259,15 @@ Game <- R6Class("Game",
           } else if (cur_trial == 3) {  # x1neg, initialize L2 & G2
             x1pos <- private$task_log[[2]][6]
             x1neg <- private$task_log[[3]][6]
-            private$task_log[[4]][1] <- as.integer(l - x1pos)  # L2
-            private$task_log[[5]][1] <- as.integer(g - x1neg)  # G2
-          } else if (cur_trial == 4) {  # L2, initialize x2pos
-            x1pos <- private$task_log[[2]][6]
-            private$task_log[[6]][1] <- as.integer(x1pos + l - L2)  # x2pos[1]
-          } else if (cur_trial == 5) {  # G2, initialize x2neg
-            x1neg <- private$task_log[[3]][6]
-            private$task_log[[13]][1] <- as.integer(x1neg + g - G2)  # Initialize x2neg
-          } else if (6 <= cur_trial & cur_trial <= 11) {  # xipos, initialize x(i+1)pos
-            xipos <- private$task_log[[cur_trial]][6]
-            private$task_log[[cur_trial + 1]][1] <- as.integer(xipos + l - L2)  # x(i+1)pos[1]
-          } else if (13 <= cur_trial & cur_trial <= 18) {  # xineg, initialize x(i+1)neg
-            xineg <- private$task_log[[cur_trial]][6]
-            private$task_log[[cur_trial + 1]][1] <- as.integer(xineg + g - G2)  # x(i+1)neg[1]
           }
         }
       },
       output_exp_result = function() {
-        result <- rep(NA, 16)
+        # result <- rep(NA, 16)
+        result <- rep(NA, 2)
         result[1] <- private$task_log[[2]][6]
-        result[9] <- private$task_log[[3]][6]
-        for (pos in 6:12) {  # index: 2-8
-          result[pos - 4] <- private$task_log[[pos]][6]
-        }
-        for (neg in 13:19) {
-          result[neg - 3] <- private$task_log[[neg]][6]
-        }
+        result[2] <- private$task_log[[3]][6]
+        names(result) <- paste0("x1", c("+","-"))
         return(result)
       },
       show_task_log = function() private$task_log,
@@ -323,7 +284,7 @@ Game <- R6Class("Game",
 # + `task_log`：$19 \times 6$ 的 list of vectors，紀錄實驗中的所有點
 # + `lotteries_box`：紀錄實驗中的所有 lotteries
 # 
-# 有 1 個 private method：`compute_value()`，根據 player 的選擇來更新下一張 lotteries 所需要的數值。  
+# 有 1 個 private method：`bisection_update()`，根據 player 的選擇來更新下一張 lotteries 所需要的數值。  
 # 有 7 個 public method：
 # 
 # + `show_setting()`：輸出 `n_trial`、`n_task` 和 `init_value`
@@ -340,13 +301,13 @@ Game <- R6Class("Game",
 ## 根據 3 個 class 之間的互動，implement sequence diagram。
 ## 可以在 `experiment()` 分別設定要不要輸出 `task_log` 和 `lotteries_box`。
 
-experiment = function(alpha, beta, lambda, phi,
-                      wp, wn, u_func = c("CRRA", "CARA"),
+experiment = function(params, #alpha, beta, lambda, wp, wn,
+                      phi, u_func = c("CRRA", "CARA"),
                       task_log=FALSE,
                       lotteries_box=FALSE) {  # Given player's attribute
   u_func <- match.arg(u_func)
   # Initialization
-  player <- Player$new(alpha, beta, lambda, phi, wp, wn, u_func)
+  player <- Player$new(params, phi, u_func)
   game <- Game$new()
   
   # Start the experiment
@@ -398,7 +359,8 @@ utility <- function(x, a, b, lambda, type = c("CRRA", "CARA")){
 
 # Playground --------------------------------------------------------------
 
-# experiment(.1, .1, 2.25, phi=NA, u_func = "CARA", F, F)
+param = c("alpha"=.88, "beta"=.88, "lambda"=2.25, "wp"=.5, "wn"=.5)
+# experiment(param, phi=NA, u_func = "CRRA", T, T)
 # experiment(.88, .88, 2.25, phi = NA, u_func = "CRRA", F, F)
 # 
 # curve(utility(x, .88, .88, 2.25, "CRRA"), from =-5, to = 5, 
@@ -406,5 +368,4 @@ utility <- function(x, a, b, lambda, type = c("CRRA", "CARA")){
 # curve(utility(x, .1, .1, 2.25, "CARA"), from =-5, to = 5 ,lwd= 2,lty=2, col = "pink", add = T)
 # 
 # points(0,0, pch=19, col = "red")
-# 
 # 
