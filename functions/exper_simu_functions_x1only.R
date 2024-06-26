52350ef55db791c7db85956e49f652bbeea6e2b4
wcnoname5 <wcnoname5@gmail.com>
2024-06-26 10:15:18 +0800

    Add slider and update bisection method in x1only fucntion

--- functions/exper_simu_functions_x1only.R
+++ functions/exper_simu_functions_x1only.R
@@ -8,32 +8,37 @@ library(tidyverse)
 Player <- R6Class("Player",
   private = list(
     # Attributes
-    params = list(), # list about utility functions
+    params = list(), # list about utility function parameters
     phi = NA, # choice consistency
     utility_function = NA,
     # Method
-    choose = function(lotteries, utility_function) {
+    choose = function(lotteries, utility_function, slider=FALSE, ...) {
       # Choose one of the lotteries depending on the player's utility function
       # Compute net utilities of the 2 lotteries
-      prospect.A <- private$compute_prospects(lotteries$A, utility_function)
-      prospect.B <- private$compute_prospects(lotteries$B, utility_function)
-      if (is.na(prospect.A)) {
-        print(lotteries$A)
-        stop("prospect.A error!")
-      } else if (is.na(prospect.B)) {
-        print(lotteries$B)
-        stop("prospect.B error!")
-      }
-      ## TODO other than difference?
-      prospect_dif <- prospect.B-prospect.A
-      
-      cho_A_prob <- 1/( 1+exp(private$phi * prospect_dif) ) 
-      choose_A <- ifelse(is.na(private$phi), prospect_dif<0,
-                   rbinom(1,1, cho_A_prob)) #dif ==0, choose A
-      if (choose_A){
-        return("A")
-      }else{
-        return("B")
+      if (!slider){ #NOT using slider, return choosen option
+        prospect.A <- private$compute_prospects(lotteries$A, utility_function)
+        prospect.B <- private$compute_prospects(lotteries$B, utility_function)
+        if (is.na(prospect.A)) {
+          print(lotteries$A)
+          stop("prospect.A error!")
+        } else if (is.na(prospect.B)) {
+          print(lotteries$B)
+          stop("prospect.B error!")
+        }
+        ## TODO other than difference?
+        prospect_dif <- prospect.B-prospect.A
+        
+        cho_A_prob <- 1/( 1+exp(private$phi * prospect_dif) ) 
+        choose_A <- ifelse(is.na(private$phi), prospect_dif<0,
+                     rbinom(1,1, cho_A_prob)) #dif ==0, choose A
+        if (choose_A){
+          return("A")
+        }else{
+          return("B")
+        }
+        
+      }else{ # if using slider, return indifferent point 
+        return(lotteries$find_optimal(params = private$params, ...)) #trial
       }
     },
     compute_prospects = function(lottery, type=c("CRRA","CARA")) {
@@ -48,7 +53,6 @@ Player <- R6Class("Player",
                                  private$params, type = type)
         loss_prospect <- utility(lottery[2],
                                  private$params, type = type)
-        ## TODO: w(.5) = .5 quite doubt
         mixed_prospect <- 
           (private$params[["wp"]] * gain_prospect) + 
           (private$params[["wn"]] * loss_prospect)  # assume no weighting function
@@ -64,9 +68,9 @@ Player <- R6Class("Player",
       private$phi <- phi 
       private$utility_function <-  match.arg(utility_function)
     },
-    input_choice = function(lotteries) {
+    input_choice = function(lotteries, slider=FALSE,...) {
       # Send player's choice to the game
-      choice <- private$choose(lotteries, private$utility_function)
+      choice <- private$choose(lotteries, private$utility_function, slider,...)
       lotteries$update_result(choice)
     }
   )
@@ -76,6 +80,7 @@ Player <- R6Class("Player",
 # 有 2 個 private methods：
 # 
 # + `choose()`：用`compute_prospects()` 算一對彩券分別的 prospects，再依softmax做選擇
+# + 若`slider`，則回傳理論值。
 # + `compute_prospects()`：根據效用函數算出該彩券的 prospect
 # 
 # 當 A 和 B 的 prospects 相同時，player 會有 50% 選 A、50% 選 B。  
@@ -83,14 +88,16 @@ Player <- R6Class("Player",
 
 
 ### 1.2 Lotteries ----
-Lotteries <- R6Class("Lotteries",
-    private = list(
+
+Lotteries <- R6Class(
+  "Lotteries",
+   private = list(
      # Attributes
      .A = rep(0L, 2), # Lottery 1
      .B = rep(0L, 2), # Lottery 2
      .result = "",
      # Methods
-     new_lotteries = function(game, lottery_values) {
+     new_lotteries = function(lottery_values) {
        # Set values of private$.A and private$.B
        private$.A <- lottery_values$.A
        private$.B <- lottery_values$.B
@@ -114,12 +121,25 @@ Lotteries <- R6Class("Lotteries",
      }
    ),
    public = list(
-     initialize = function(game, lottery_values) {
-       # given_values <- c(x(i-1)pos, xipos_(j-1), L2, G2)
-       private$new_lotteries(game, lottery_values)
+     initialize = function(lottery_values) {
+       private$new_lotteries(lottery_values)
      },
      update_result = function(choice) {
        private$.result <- choice
+     },
+     find_optimal = function(params, trial){
+       if (trial==1){
+         result <- -((utility(self$A[1], params, "CRRA")*
+                        (params$wp/params$wn))/params$lambda)^(1/params$beta)
+         
+       } else if(trial %in% c(2,3)){
+         result <- params$wp * utility(self$A[1], params, "CRRA")+
+           params$wn*utility(self$A[2], params, "CRRA")
+         if (result>=0) result <- result^(1/params$alpha)
+         else result <- -((1/params$lambda)*(-result))^(1/params$alpha)
+         
+       }
+       return(round(result))
      }
    )
 )
@@ -129,7 +149,6 @@ Lotteries <- R6Class("Lotteries",
 # 有 1 個 private method：`new_lotteries()`，會根據目前的 trial 和 task 來產生相對應的兩張 lotteries。
 # 該 method 在創建物件時被呼叫。  
 # 有 1 個 public method：`update_result()`，讓 player 把答案寫在 lotteries 上。  
-
 
 
 ###1.3 Game----
@@ -143,41 +162,95 @@ Game <- R6Class(
     n_trial = 3L,  #no. of estimation. 19L = 5+ 7 + 7
     n_task = 5L, # times of bisection
     task_log = NA,  # list of vectors, should be initialized
-    lotteries_box = NA,  # 2D list, should be initialized
-    scroll = FALSE, # using scroll bar after `n_trial` bisections
+    slider = FALSE, # using slider after `n_trial` bisections
+    bound = list(), # use to 
     # Methods
-    ## bisection
-    bisection_update = function(choice, value_list, cur_task, cur_trial,
-                                scroll=FALSE) {
-      # choice <- "A" or "B"
-      # value_list: e.g., x1pos_list
-      # TODO: modify scroll bar
-      last_est <- value_list[cur_task] #x_{i-1}
-      if  (scroll & cur_task == private$n_task){
-        interv <-  c(last_est, 0)# interval
-        traget <- runif(1, interv[0], interv[1]) # a probability generator
-      }else{
-        # TODO: modify biesction s.t. starting value affect less
-        step <- abs(value_list[1]) # x_0 
-        # upbd <- 2000 # upper bound of step
-        # step <- max(abs(value_list[1]), upbd) 
-        change <- ((1 / 2) ^ cur_task) * step
+    ## bisection: suppose x in [x0, x0+delta]
+    ## iterate next point by delta/(2^(j+1)) each time
+    bisection_update = function(choice, #A,B/ numuric if use slider
+                                bisect_log, cur_task, cur_trial,
+                                phi) {
+      last_est <- bisect_log[cur_task] #x_{i-1}
+      if  (is.numeric(choice)){ # use slider
+        interv <- private$bound[[cur_trial]][private$n_task, ]
+        int.range <- abs(interv[2]-interv[1])
+        # expand interval by 3 times wider
+        interv <- interv + c(-int.range, int.range)
+        ## Probability generator
+        if (is.na(phi)){
+          target <- choice |> as.integer()
+        }else{
+          target <- rnorm(1, mean = choice, sd = (1/phi)*5)|>
+            as.integer()
+          ## boundary conditions
+          if ((target >= interv[1]) & (target <= interv[2])){
+            target <- target
+          }else if (target < interv[1]){
+            target <- interv[1]
+          }else if (target > interv[2]){
+            target <- interv[2]
+          }
+        }
+      }else{ # original bisection
+        delta <- abs(private$bound[[cur_trial]][1,2] -
+                       private$bound[[cur_trial]][1,1])  #compute delta
+        step <- (((1 / 2) ^ cur_task) * delta) |>
+          round() # update step
         
         if (cur_trial == 1) {  # estimate L
-          change <- -change
-        }
+          change <- list("A" = c(0, -step), "B" = c(step, 0))
+        }else{
+          change <- list("A" = c(step, 0), "B" = c(0, -step))
+        } 
+        # update
+        private$bound[[cur_trial]][cur_task+1,] <-
+          private$bound[[cur_trial]][cur_task,] + change[[choice]]
         
-        if (choice == "A") {
-          target <- last_est + change
-        } else {
-          target <- last_est - change
-        }
-        
+        target <- private$bound[[cur_trial]][cur_task+1,] |> 
+          mean()  
         target <- as.integer(round(target / 5) * 5)  # Make target divisible by 5
+        # target <- as.integer(round(target))  # Make target integer
+        # cat(c(cur_trial,cur_task), "choice:",choice, "; update:", target,"\n")
       }
         return(target) # return bisection iteration value
-    }
+    },
     ## PEST
+    PEST_update = function(choice,
+                           bisect_log, cur_task, cur_trial,
+                           phi){
+      G <- 2000
+      L <- runif(1,-5 * G, 0)
+      .step <- 640 # 10 - 1280
+      
+      last.choose <- c(NA, NA, NA, NA) # last 4 choose history
+      while (step != 10) {
+        # choosing lotteries
+        chosen <- ifelse(choice, "A", "B")
+        last.choose <- c(last.choose, chosen)[-1]
+        
+        if (last.choose[3] != last.choose[4]) {
+          # reversal
+          .step <- .step / 2
+          # reversal after double
+          if (all(last.choose[1:3] == ("A")) |
+              all(last.choose[1:3] == ("B"))) {
+            extra.step <- TRUE
+          }
+        }
+        # double 
+        else if (all(last.choose[2:4] == ("A")) |
+                 all(last.choose[2:4] == ("B"))) {
+          if (extra.step) {
+            .step <- .step
+            extra.step <- FALSE
+          } else{
+            .step <- 2 * .step
+          }
+        }
+        L <- L + (ifelse(choose.A, 1,-1) * .step)
+        
+      }
+    }
   ),
   ## Public
   public = list(
@@ -188,22 +261,27 @@ Game <- R6Class(
                         c("G"= 2000L, "L"=-2000L,
                           "g"=300L, "l"=-300L, "x1+"=1000L)
                  ),
-               scroll = FALSE) {
+               slider = FALSE) {
         n_trial <- private$n_trial
         n_task <- exp_params$n_task
         private$n_task <- n_task
         private$init_values <- exp_params$init_values
-        private$scroll <- scroll
+        private$slider <- slider
         
         # Initialize task_log
         private$task_log <- matrix(NA, ncol = n_trial, nrow = n_task + 1L,
                                    dimnames = list(c(), c("L", "x1pos","x1neg")))
-        private$task_log[1, 1] <- private$init_values["L"] # -2000
-        private$task_log[1, 2] <- private$init_values["x1+"] # 1000
         
-        # Initialize lotteries_box
-        private$lotteries_box <- as.list(rep(NA, n_trial * n_task))
-        dim(private$lotteries_box) <- c(n_trial, n_task)
+        ## The bisection method: suppose x in [x0, x0+delta]
+        ## delta for L is 2*G
+        private$bound <- lapply(1:3, \(x)(matrix(nrow = (n_task+1),
+                                                 ncol = 2)))
+        names(private$bound) <- c("L", "x1pos", "x1neg")
+        private$bound[["L"]][1,] <-  c(-2*private$init_values[["G"]], 0)
+        private$bound[["x1pos"]][1,] <-  c(0, private$init_values[["G"]])
+        #choose midpoint as start point
+        private$task_log[1, 1] <- private$bound[["L"]][1,] |> mean() # -2000
+        private$task_log[1, 2] <- private$bound[["x1pos"]][1,] |> mean() # 1000
       },
     
     show_setting = function() {
@@ -226,25 +304,24 @@ Game <- R6Class(
         .A <- c(G, 0)
         .B <- rep(self$show_task_log()[cur_task, "x1pos"], 2)
       } else if (cur_trial == 3) {  # x1neg
-        L <- self$show_task_log()[private$n_task, "L"]
+        L <- self$show_task_log()[private$n_task+1, "L"]
         .A <- c(0, L)
         .B <- rep(self$show_task_log()[cur_task, "x1neg"], 2)
       }
       
       lottery_values <- list(".A" = .A, ".B" = .B)
-      new_lotteries <- Lotteries$new(self, lottery_values)
+      new_lotteries <- Lotteries$new(lottery_values)
       return(new_lotteries)
     },
     
-    update_lotteries_box = function(lotteries, cur_trial, cur_task) {
-      private$lotteries_box[[cur_trial, cur_task]] <- lotteries
-    },
-    
-    update_task_log = function(choice, cur_trial, cur_task) {
+    update_task_log = function(choice, cur_trial, cur_task, phi) {
       # The function takes the choice of players, does the computation, and update the task log.
       # Update the current value
-      target_list <- private$task_log[, cur_trial]
-      value <- private$bisection_update(choice, target_list, cur_task, cur_trial, scroll=FALSE) 
+      bisec_pts <- private$task_log[, cur_trial]
+      # use_slider <- (private$slider & cur_task==private$n_task) 
+      value <- private$bisection_update(choice, bisec_pts, cur_task, cur_trial,
+                                        # use_slider = use_slider,
+                                        phi=phi) 
       private$task_log[cur_task + 1, cur_trial] <- value
       
       # Update next trial's initial value
@@ -254,6 +331,7 @@ Game <- R6Class(
 
         if (cur_trial == 2) {  #end of x1+, initialize x1neg[1]
           L <- private$task_log[(private$n_task+1), "L"]
+          private$bound[["x1neg"]][1, ] <- c(L, 0)
           private$task_log[1, "x1neg"] <-
             as.integer(round(floor(L * .5) / 5) * 5)  
         } else if (cur_trial == 3) {  # x1neg, initialize L2 & G2
@@ -271,29 +349,27 @@ Game <- R6Class(
       return(result)
     },
     show_task_log = function() private$task_log,
-    show_lotteries_box = function() private$lotteries_box
+    show_bound = function() private$bound
   )
 )
 
 
 # `Game` 有 5 個 private attributes：
-# 
+# + `exp_params`: A list, with `n_task`, `init_value` two vectors
 # + `init_value`：$G$、$L$、$g$、$l$
 #   + `n_trial`
 # + `n_task`
-# + `task_log`：$19 \times 6$ 的 list of vectors，紀錄實驗中的所有點
-# + `lotteries_box`：紀錄實驗中的所有 lotteries
+# + `task_log`: $19 \times 6$ 的 list of vectors，紀錄實驗中的所有點
+# + `slider` (logical): using slider to choose indifferent point after `n_task` bisection  
 # 
 # 有 1 個 private method：`bisection_update()`，根據 player 的選擇來更新下一張 lotteries 所需要的數值。  
 # 有 7 個 public method：
 # 
 # + `show_setting()`：輸出 `n_trial`、`n_task` 和 `init_value`
 # + `generate_lotteries()`：根據目前的 trial 和 task 產生相對應的 lottery
-# + `update_lotteries_box()`：把 lotteries 和 player 的選擇，都一起儲存下來
 # + `update_task_log()`：player 選擇之後，計算下一張 lottery 會用到的值，並且紀錄下來
 # + `output_exp_result()`：實驗結束後，把 $x^+_1$ 到 $x^+_8$、$x^-_1$ 到 $x^-_8$ 儲存起來，並以 1d vector 的形式輸出
 # + `show_task_log()`：輸出 `task_log`，輸出格式是 list of vector
-# + `show_lotteries_box()`：輸出 `lotteries_box`，輸出格式是 `dim = 2` 的 list
 
 
 # 2. Define `experiment()` ------------------------------------------------
@@ -308,33 +384,35 @@ experiment = function(params, #alpha, beta, lambda, wp, wn,
                                         c("G"= 2000L, "L"=-2000L,"g"=300L, "l"=-300L, "x1+"=1000L)
                                       ),
                       phi, u_func = c("CRRA", "CARA"),
-                      task_log=FALSE,
-                      lotteries_box=FALSE) {  # Given player's attribute
+                      slider = FALSE,
+                      task_log=FALSE) {  # Given player's attribute
   u_func <- match.arg(u_func)
   # Initialization
   player <- Player$new(params, phi, u_func)
-  game <- Game$new(exp_params=exp_params)
-  
+  game <- Game$new(exp_params=exp_params, slider = slider)
   # Start the experiment
   for (trial in 1:game$show_setting()[1]) { # no. of estimated quantities
+    # theoretical True value
     for (task in 1:game$show_setting()[2]) { # no. of bisection
       cur_lotteries <- game$generate_lotteries(trial, task) # (->Lottery class object)
-      player$input_choice(cur_lotteries) # update lottery by player's choice
-      game$update_lotteries_box(cur_lotteries, trial, task)
-      game$update_task_log(cur_lotteries$result, trial, task) # bisection
+      # update lottery by player's choice
+      player$input_choice(cur_lotteries,
+                          slider =
+                            (slider & task==exp_params$n_task),
+                          trial = trial) 
+      game$update_task_log(cur_lotteries$result, trial, task, phi = phi) # bisection
+      ## debug
+      # if (task == game$show_setting()[2]){
+      #   print(game$show_bound()[[trial]])
+      # }
     }
   }
   
   # Finish the experiment
   exp_result <- game$output_exp_result()
-  if (task_log | lotteries_box) {
+  if (task_log) {
     exp_result <- list("estimates" = exp_result)
-    if (task_log) {
-      exp_result$log <- game$show_task_log()
-    }
-    if (lotteries_box) {
-      exp_result$lotteries <- game$show_lotteries_box()
-    }
+    exp_result$log <- game$show_task_log()
   }
   return(exp_result)
 }
@@ -369,18 +447,5 @@ utility <- function(x, params, type = c("CRRA", "CARA")){ #params be a list
               "CARA" = CARA(x, mu = mu, nu = nu, lambda= lambda))
   return(ss)
 }
-
-# Playground --------------------------------------------------------------
-
-# param = c("alpha"=.88, "beta"=.88, "lambda"=2.25, "wp"=.5, "wn"=.5)
-# experiment(param, phi=NA, u_func = "CRRA", T, T)
-# experiment(.88, .88, 2.25, phi = NA, u_func = "CRRA", F, F)
-# 
-# curve(utility(x, .88, .88, 2.25, "CRRA"), from =-5, to = 5, 
-#       lwd= 2, col = "lightblue")
-# curve(utility(x, .1, .1, 2.25, "CARA"), from =-5, to = 5 ,lwd= 2,lty=2, col = "pink", add = T)
-# 
-# points(0,0, pch=19, col = "red")
-# 
 
   
